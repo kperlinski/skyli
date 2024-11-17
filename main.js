176e0a6645ad4278942fdd4966d988baf161460c
Konrad <konradperlinski0@gmail.com>
2024-11-17 21:48:36 +0100

    Funkcja laczenia sie ze strona pogody z przesylanie danych

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ async function search() {
     const searchBox = document.querySelector("#search input");
     NameC = searchBox.value;
     checkWeather(NameC);
-
 }
 
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
